391616fd378ae5016aab556de845df770c43f870
John Petersson <johnpetersson@gmail.com>
2018-05-29 17:47:20 +0200

    Removed unused

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 ReactDOM.render(
-<ParallaxProvider>
   <App />
-</ParallaxProvider>
   , document.getElementById('root'));
 registerServiceWorker();
